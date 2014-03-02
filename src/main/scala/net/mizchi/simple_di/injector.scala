6888f52043497d0c0cfcf1c6b94cfb0cf85b9e15
Koutarou Chikuba <miz404@gmail.com>
2014-03-02 11:19:49 +0900

    Add keyword map

--- src/main/scala/net/mizchi/simple_di/injector.scala
+++ src/main/scala/net/mizchi/simple_di/injector.scala
@@ -4,37 +4,45 @@ import scala.reflect.{classTag, ClassTag}
 object Injector {
   private val rootInjector =  new Injector(None)
   def createInjector =  rootInjector.createChild(rootInjector)
-  def mapValue[T:ClassTag](instance:T): Unit =
-    rootInjector.mapValue[T](instance)
+
+  def mapValue[T:ClassTag](instance:T, key:String = ""): Unit =
+    rootInjector.mapValue[T](instance, key)
   def clear = rootInjector.clear
 }
 
 class Injector(val parent:Option[Injector]) {
   private val valueMap =
-    scala.collection.mutable.Map[ClassTag[_], Any]()
+    scala.collection.mutable.Map[String, scala.collection.mutable.Map[ClassTag[_], Any]]()
 
   def clear = valueMap.clear
   def createChild:Injector = new Injector(Some(this))
   def createChild(parent:Injector):Injector = new Injector(Some(parent))
-
-  private def hasInjectedValue(cTag:ClassTag[_]):Boolean = {
-    valueMap.get(cTag) match {
-      case Some(_) => true
-      case None => false
+  def mapValue[T:ClassTag](instance:T, key:String): Unit =
+    valueMap.get(key) match {
+      case Some(m) =>
+        m += classTag[T] -> instance
+      case None =>
+        valueMap += key -> scala.collection.mutable.Map[ClassTag[_], Any]()
+        mapValue[T](instance, key)
     }
-  }
 
-  def mapValue[T:ClassTag](instance:T): Unit = {
-    valueMap += classTag[T] -> instance
-  }
-
-  def getInstance[T:ClassTag]: T =
-    valueMap.get(classTag[T]) match {
-      case Some(i) => i.asInstanceOf[T]
+  def getInstance[T:ClassTag]: T = this.getInstance[T]("")
+  def getInstance[T:ClassTag](key:String): T =
+    valueMap.get(key) match {
+      case Some(map) =>
+        map.get(classTag[T]) match {
+          case Some(instance) => instance.asInstanceOf[T]
+          case None => parent match {
+            case Some(j:Injector) => j.getInstance[T](key)
+            case None => throw new Error("no injected target error(instance map)" + key)
+          }
+        }
       case None => {
         parent match {
-          case Some(j) => j.getInstance[T]
-          case None => throw new Error("no injected target error")
+          case Some(k) => k.getInstance[T](key)
+          case None =>{
+            throw new Error("no injected target error(key map)" + key)
+          }
         }
       }
     }
@@ -42,10 +50,12 @@ class Injector(val parent:Option[Injector]) {
 
 trait Inject {
   var injector:Injector = Injector.createInjector
-  def injected[T:ClassTag]: T = injector.getInstance[T]
+  def injected[T:ClassTag](key:String): T = injector.getInstance[T](key)
+  def injected[T:ClassTag]: T = this.injected[T]("")
 }
 
 trait CustomInject {
   val injector:Injector
-  def injected[T:ClassTag]: T = injector.getInstance[T]
+  def injected[T:ClassTag](key:String): T = injector.getInstance[T](key)
+  def injected[T:ClassTag]: T = this.injected[T]("")
 }
--- src/test/scala/AppSpec.scala
+++ src/test/scala/AppSpec.scala
@@ -2,6 +2,8 @@ import org.specs2.mutable._
 import net.mizchi.simple_di._
 
 class InjectorSpec extends Specification {
+  Injector.clear
+
   class A(val x:Int)
   class B
   class C extends Inject {
@@ -15,5 +17,22 @@ class InjectorSpec extends Specification {
 
   override def is = s2"""
   C must have injected instance with its property ${ c.a.x mustEqual 4 }
+  """
+}
+
+class Injector2Spec extends Specification {
+  Injector.clear
+  class A(val x:Int)
+
+  class I extends Inject {
+    val a: A = injected[A]("foo")
+  }
+  Injector.mapValue(new A(3))
+  Injector.mapValue(new A(4), "foo")
+  val i = new I
+
+  //i must have injected instance by keyword ${ i.a.x mustEqual 4 }
+  override def is = s2"""
+  I must have injected instance with its property ${ i.a.x mustEqual 4 }
   """
 }
